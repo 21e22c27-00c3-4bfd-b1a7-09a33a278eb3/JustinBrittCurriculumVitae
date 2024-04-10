4d911261102244dd0a5002995d6786f0dcd025f0
Justin Britt <britt@uwindsor.ca>
2024-04-10 16:16:52 -0400

    Changes to work experience

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import { generalData } from "@/data/general";
 import { educationData } from "@/data/education";
 import type { EducationContent } from "@/data/education";
 
+import { workExperienceData } from "@/data/workExperience";
+import type { WorkExperienceContent } from "@/data/workExperience";
+
 type EducationContentProps = EducationContent;
+type WorkExperienceContentProps = WorkExperienceContent;
 
 const EducationContent: React.FC<EducationContentProps> = ({ title, items }) => {
     return (
@@ -23,6 +27,36 @@ const EducationContent: React.FC<EducationContentProps> = ({ title, items }) => 
                                 <h4>{item.title}</h4>
                                 <p className="text-slate-600 dark:text-gray-400">
                                     {item.subTitle}, {item.location}
+                                </p>
+                                {item.description ? (
+                                    <p className="text-slate-600 dark:text-gray-400 mt-2">
+                                        {item.description}
+                                    </p>
+                                ) : null}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+const WorkExperienceContent: React.FC<WorkExperienceContentProps> = ({ title, items }) => {
+    return (
+        <section className="my-14 text-sm">
+            <h3 className="mb-6">{title}</h3>
+            <div className="flex flex-col gap-6">
+                {items.map((item, index) => {
+                    return (
+                        <div className="flex" key={index}>
+                            <div className="mr-8 max-w-[100px] w-full text-slate-400 dark:text-slate-400">
+                                {item.date}
+                            </div>
+                            <div className="flex flex-col flex-1">
+                                <h4>{item.title}</h4>
+                                <p className="text-slate-600 dark:text-gray-400">
+                                    {item.subTitle}
                                 </p>
                                 {item.description ? (
                                     <p className="text-slate-600 dark:text-gray-400 mt-2">
@@ -83,6 +117,10 @@ export default function Home() {
         ) : null}
         {educationData.map((educationContent, index) => {
           return <EducationContent {...educationContent} key={index} />;
+        })}
+
+        {workExperienceData.map((workExperienceContent, index) => {
+          return <WorkExperienceContent {...workExperienceContent} key={index} />;
         })}
 
         <section className="my-14 text-sm">
--- data/workExperience.ts
+++ data/workExperience.ts
@@ -2,24 +2,12 @@ export const workExperienceData = [
     {
         title: "Work Experience",
         items: [
-            //{
-            //    title: "Doctor of Philosophy in Industrial and Manufacturing Systems Engineering",
-            //    subTitle: "University of Windsor",
-            //    date: "October 2023",
-            //    description: "",
-            //},
-            //{
-            //    title: "Master of Applied Science in Industrial Engineering",
-            //    subTitle: "University of Windsor",
-            //    date: "June 2016",
-            //    description: "",
-            //},
-            //{
-            //    title: "Bachelor of Science in Physics",
-            //    subTitle: "The University of Winnipeg",
-            //    date: "June 2010",
-            //    description: "",
-            //},
+            {
+                title: "Sessional Instructor",
+                subTitle: "Cape Breton University",
+                date: "September 2023 - April 2024",
+                description: "",
+            },
         ],
     }
 ];
