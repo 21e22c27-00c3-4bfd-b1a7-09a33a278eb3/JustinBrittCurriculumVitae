25d180d557aef8fbb90ec9352f80cd6ad55132d8
Justin Britt <britt@uwindsor.ca>
2024-04-13 14:16:37 -0400

    Changes to citation

--- data/publications.ts
+++ data/publications.ts
@@ -3,8 +3,8 @@ export const publicationsData = [
         title: "Publications",
         items: [
             {
-                citation: "",
-                type: "",
+                citation: "Britt, J., Azab, A., Baki, M. F., & Bin Hassan, R. (2023). Optimizing operating room planning and scheduling using robust optimization and fuzzy hyperheuristics: addressing the case mix problem. May 29-31, 2023, CORS Optimization Days at HEC Montreal, Montreal, Canada.",
+                type: "Abstract",
             },
         ],
     }
