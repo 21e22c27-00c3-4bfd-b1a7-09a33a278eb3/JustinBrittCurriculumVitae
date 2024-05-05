675d45e3b62567c1c105dcae82332cc23933c0b0
JustinBritt <britt@uwindsor.ca>
2024-05-05 10:58:08 -0400

    Removed commas

--- data/workExperience.ts
+++ data/workExperience.ts
@@ -7,28 +7,28 @@ export const workExperienceData = [
                 employer: "Cape Breton University",
                 location: "Sydney, Nova Scotia, Canada",
                 date: "September 2023 - April 2024",
-                description: "Taught post-baccalaureate level business statistics (MGSC-5108) and operations management (MGSC-5128) courses",
+                description: "Taught post-baccalaureate level business statistics (MGSC-5108) and operations management (MGSC-5128) courses"
             },
             {
                 title: "Postdoctorate Fellow",
                 employer: "Production & Operations Management Research Lab",
                 location: "Windsor, Ontario, Canada",
                 date: "September 2023 - October 2023",
-                description: "",
+                description: ""
             },
             {
                 title: "Research Assistant",
                 employer: "Production & Operations Management Research Lab",
                 location: "Windsor, Ontario, Canada",
                 date: "May 2015 - August 2023",
-                description: "Developed mathematical programming models, discrete event simulation models, solution methods (hyperheuristics and metaheuristics), and a decision support system for strategic and tactical operating room planning problems",
+                description: "Developed mathematical programming models, discrete event simulation models, solution methods (hyperheuristics and metaheuristics), and a decision support system for strategic and tactical operating room planning problems"
             },
             { 
                 title: "Graduate Assistant",
                 employer: "University of Windsor",
                 location: "Windsor, Ontario, Canada",
                 date: "September 2015 - May 2023",
-                description: "Conducted weekly tutorial sessions, assisted with grading, held weekly office hours, and responded to students' questions",
+                description: "Conducted weekly tutorial sessions, assisted with grading, held weekly office hours, and responded to students' questions"
             },
         ],
     }
