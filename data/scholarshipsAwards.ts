457bba572d553eaa70dd492f0a33d04346fe1862
JustinBritt <britt@uwindsor.ca>
2024-05-05 16:34:13 -0400

    Added awards

--- data/scholarshipsAwards.ts
+++ data/scholarshipsAwards.ts
@@ -22,6 +22,26 @@ export const scholarshipsAwardsData = [
                 scholarshipAward: "University of Windsor Graduate Student Bursary",
                 date: "2015, 2021"
             },
+            {
+                scholarshipAward: "Joeleene McBride Endowed Scholarship",
+                date: "2013 - 2014"
+            },
+            {
+                scholarshipAward: "Charles G. Tullis Endowed Scholarship",
+                date: "2012 - 2013"
+            },
+            {
+                scholarshipAward: "President's Honor List - Fall 2012, Winter 2013, Winter 2014, Spring 2014",
+                date: "2012 - 2014"
+            },
+            {
+                scholarshipAward: "Bulldog Out of State Scholarship",
+                date: "2012 - 2014"
+            },
+            {
+                scholarshipAward: "Dean's Honor List - All Eligible Quarters",
+                date: "2011 - 2014"
+            },
         ],
     }
 ];
